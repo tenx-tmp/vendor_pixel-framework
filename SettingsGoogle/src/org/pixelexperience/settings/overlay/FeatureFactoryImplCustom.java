9d4b20dc200132e351fcb237db8a6b218f732492
someone5678 <nemui3353@gmail.com>
2022-10-24 19:45:57 +0900

    SettingsGoogle: Support  FeatureFactoryImpl

---
+++ SettingsGoogle/src/org/pixelexperience/settings/overlay/FeatureFactoryImplCustom.java
@@ -0,0 +1,11 @@
+package org.pixelexperience.settings.overlay.FeatureFactoryImplCustom;
+
+import android.content.Context;
+
+import com.android.settings.overlay.FeatureFactoryImpl;
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.android.settings.accounts.AccountFeatureProvider;
+
+public final class FeatureFactoryImplCustom extends FeatureFactoryImpl {
+
+}
